3300dff8f16426f7ee13e08cf2a839f8daef59e6
Touchy00 <Touchy00@yandex.ru>
2024-03-04 23:39:59 +0300

    fix

--- MyCalculator.java
+++ MyCalculator.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 
 public class MyCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        calc();
+    }
+    static void calc(){
         Scanner sc = new Scanner(System.in);  //задается новое имя сканнера.Сканнер отправляет запрос/сигнал в терминал и ожидает ответа пользователя
         System.out.println("Введите значиние"); //приветствие для пользователя, чтобы пошла ответная реакция
-        String value1 = sc.next();   // получает значение которое ввожит пользователь
-        String operator = sc.next();
-        String value2 = sc.next();
+        String line = sc.nextLine();
+        String [] values = line.split(" ");
+        if (values.length != 3){
+            throw new NumberFormatException("Нельзя вводить больше 2 значений");
+        }
+        String value1 = values[0];   // получает значение которое ввожит пользователь
+        String operator = values[1];
+        String value2 = values[2];
         boolean isArab = false;   // флаг который указывает, что пользователь ввожил арабские числа
         int res = 0;// переменная в которую помещаем результат сложения, умножения, деления, вычитания
         int num1;
@@ -15,14 +23,13 @@ public class MyCalculator {
         num1 = m.transrim(value1); //переводит из римской цифры в  арабскую  1=I, 2=II, если нет такой римской цифры возрвщвет 0
         num2 = m.transrim(value2);
         if (num1 == 0 | num2 == 0) { //если value1 или value2 не римское число, то n1 или n2 равно 0
-            try { //весь код, который может вызвать исключения,находится в блоке трай
+            if (value1.matches("\\d+") && value2.matches("\\d+")) { //весь код, который может вызвать исключения,находится в блоке трай
                 num1 = Integer.parseInt(value1); //переводит из прописной стринг(например "2") в интовую (2)
                 num2 = Integer.parseInt(value2); //переводит из прописной стринг(например "2") в интовую (2)
                 isArab = true; // ответ-числовой если фолс-ответ римскими
 
-            } catch (NumberFormatException e) { // елли value1 value2 не арабское число (напмире "Даша / Маша")
-                System.out.println(value1 + " или " + value2 + " не является арабской цифрой !");
-                return;
+            } else{ // елли value1 value2 не арабское число (напмире "Даша / Маша")
+                throw new NumberFormatException(value1 + " или " + value2 + " не является арабской цифрой !");
             }
         }
         if (num1 >=1 && num1 <=10 && num2 >=1 && num2 <=10) {//условия калькулятора(1-10)будут работать только, в рамках одновременного обязательства
@@ -39,17 +46,16 @@ public class MyCalculator {
                 res = num1/num2;
             }
         }else{ //если (if) не подходит, тогда сработает (else) иначе,которое выведет рпботу кода в асболютный ответ/тупик
-            System.out.println("Так делать не нааадо. Можно от 1-10");
-            return; // завершает метод
+            throw new NumberFormatException("Так делать не нааадо. Можно от 1-10");
         }
         if (isArab){//проверка на арабское число имеет значение что было
             System.out.println(res); //печатает соответствующий арабский ответ
         }else { //если исараб-фолс то печатает римский ответ
-            try {
+            if(res > 0) {
                 String resArab = m.transArab(res); // Переводит из арбского в римское число
                 System.out.println(resArab); // Печатает РИМСКИЙ ответ
-            }catch (ArrayIndexOutOfBoundsException e){
-                System.out.println("Отрицательных римских значений не существует " + e);
+            }else {
+                throw new ArrayIndexOutOfBoundsException("Отрицательных римских значений не существует ");
             }
         }
 
@@ -81,6 +87,3 @@ public class MyCalculator {
         return slova[res - 1];
     }
 }
-
-
-
